828a9619830805a6614e91f6c3bdb88649f69ac7
lunnaviana05 <lunnaviana05@gmail.com>
2023-02-08 16:35:43 +0000

    Gravar Todos os Comandos

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,13 @@
 public class Variaveis {
-    
+    public static void main(String[] args){
+        int idade=30;
+        byte quantidadesdeBolas=7;
+        char letras='N';
+        boolean vivo=false;
+        double salario= 1302;
+        float salariomenor= 1212;
+        shot alunoPalmital= 10000;
+        long populaçãoPalmital= 22000;
+        System.out.println(idade);
+    }
 }
